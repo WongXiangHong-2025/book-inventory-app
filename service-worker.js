b1bab1a07dd681c9f12739a7be58f0b87cb0a411
WONG XIANG HONG <25060693@siswa.um.edu.my>
2026-07-30 23:28:41 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'sbc-inventory-v6';
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest?v=6',
+  './css/styles.css?v=6',
+  './js/storage.js?v=6',
+  './js/metadata.js?v=6',
+  './js/scanner.js?v=6',
+  './js/app.js?v=6',
+  './icons/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => cachedResponse || fetch(event.request))
+  );
+});
